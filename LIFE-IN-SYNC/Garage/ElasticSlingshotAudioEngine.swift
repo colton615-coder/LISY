5b9e8f4fcaf3f9290d97ba30151e2cbbc31c51d1
Colton Thomas <colton615@gmail.com>
2026-06-11 22:46:47 -0400

    Add follow-through pitch handling

--- LIFE-IN-SYNC/Garage/ElasticSlingshotAudioEngine.swift
+++ LIFE-IN-SYNC/Garage/ElasticSlingshotAudioEngine.swift
@@ -1443,6 +1443,8 @@ private final class ElasticSlingshotRenderState {
             return 880
         case .downswing, .impact, .loopDelay, .finished:
             return 0
+        case .followThrough(progress: let progress):
+            <#code#>
         }
     }
 
